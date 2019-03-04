7d75d5d4bd0654d98c3b4b3d4092f4c76c949552
Stavris G <stavris@cvi136231.ugr.es>
2019-03-04 15:10:06 +0100

    Creating the heatmap based on the top n genes of the given matrix, implementing custom title for the heatmap, omit x and y legends

--- visualisation.R
+++ visualisation.R
@@ -1,7 +1,7 @@
 ### Stavros Giannoukakos ###
 
-### Notes: Script arguments: 1) <input matrix>.mat 2) <cs list of the groups> 3) path/to/project_folder 4) baseName 5) percentage/top_n_genes 6) path/to/packrat_directory
-### Example Command: Rscript visualisation.R matfile.mat cell,cell,cell,exosomes,exosomes,exosomes /projects/project_name mature_sense_minExpr5_RCadj 0.1 /projects/packrat
+### Notes: Script arguments: 1) <input matrix>.mat 2) <cs list of the groups> 3) path/to/project_folder 4) baseName 5) percentage/top_n_genes 6) title of heatmap within quotation marks
+### Example Command: Rscript visualisation.R matfile.mat cell,cell,cell,exosomes,exosomes,exosomes /projects/project_name mature_sense_minExpr5_RCadj 0.1 "Top Genes"
 
 # Input arguments and error control
 args <- commandArgs(TRUE)
@@ -19,13 +19,9 @@ if (length(args) == 4) {
   }
   outdir <- args[3]  # Output directory
   basename <- args[4]  # Base name
-  n_top <- 0.1  # # Cutoff for plotting the 10% most variable genes
-  # if (dir.exists("/opt/sRNAtoolboxDB/packrat")) {
-  #   packrat_path <- "/opt/sRNAtoolboxDB/packrat"  # Alu/Epigenoma
-  # } else {
-  #   cat("ERROR - The packrat directory could not be found...\nEXITING!\n")
-  #   quit()
-  # }
+  n_top <- 0.1  # Cutoff for plotting the 10 top genes
+  heatmap_title <- ""
+
 } else if (length(args) == 5) {
   if (!file.exists(args[1])) {
     cat("ERROR - The input matrix does NOT exist...\nEXITING!\n")
@@ -41,25 +37,40 @@ if (length(args) == 4) {
   outdir <- args[3]  # Output directory
   basename <- args[4]  # Base name
   n_top <- as.numeric(args[5])  # Cutoff for plotting the top_n most variable genes
-  # packrat_path <- args[6]  # Path of packrat environment
-} else {
-  cat("ERROR - The number of input arguments is not correct...\nEXITING!\n")
+  heatmap_title <- ""
+  
+} else if (length(args) == 6) {
+if (!file.exists(args[1])) {
+  cat("ERROR - The input matrix does NOT exist...\nEXITING!\n")
   quit()
 }
+matfile <- read.delim(args[1], header=TRUE, row.names=1)   # Input the input delimited text file containing the count matrix
+groups <- unlist(strsplit(args[2], ","))  # Sample description
+sampletypevalues <- rev(unique(groups))  # Getting the group levels
+if (!dir.exists(args[3])) {
+  cat("ERROR - The output directory does NOT exist...\nEXITING!\n")
+  quit()
+}
+outdir <- args[3]  # Output directory
+basename <- args[4]  # Base name
+n_top <- as.numeric(args[5])  # Cutoff for plotting the top_n most variable genes
+heatmap_title <- toString(args[6])
 
-# packrat_path <- "/Users/stavris/R/packrat"
+} else {
+cat("ERROR - The number of input arguments is not correct...\nEXITING!\n")
+quit()
+}
+
+  
+
 # matfile <- read.delim("/Users/stavris/R/projects/example_project/example_matrix.mat", header=TRUE, row.names=1)
 # groups <- unlist(strsplit("cell,cell,cell,exosomes,exosomes,exosomes", ","))
 # sampletypevalues <- rev(unique(groups))
 # outdir <- "/Users/stavris/R/projects/example_project"
 # basename <- "mature_sense_minExpr5_RCadj"
-# n_top <- 0.125
-# n_top <- 25
+# n_top <- 20
+# heatmap_title <- toString("Top Genes")
 
-# Initiating packrat environment and 
-# loading all necessary libraries
-# library("packrat")
-# packrat::init(packrat_path)
 library("edgeR")
 library("plotly")
 library("heatmaply")
@@ -108,47 +119,36 @@ plotMDS(data, col=col_condition)
 title(main = "MultiDimensional Scaling plot\n(distances approximate the log2 fold changes between the samples)")
 dev.off()
 
-# Estimate the variance for each row in the log2counts matrix
-var_genes <- apply(log2counts, 1, var)
-
-
 if (check.integer(n_top)) {
-  # Obtaining the percentage of top most variable genes
-  select_var <- names(sort(var_genes, decreasing=TRUE))[1:n_top]
+  # Obtaining the percentage of top genes
+  top_genes <- matfile[1:n_top, ]
 } else {
-  # Obtaining the n top most variable genes
-  select_var <- names(sort(var_genes, decreasing=TRUE))[1:(nrow(log2counts)*n_top)]
+  # Obtaining the n top genes
+  top_genes <- matfile[1:(nrow(log2counts)*n_top), ]
 }
 
-# Subset log2counts matrix
-highly_variable <- log2counts[select_var, ]
 
 # Creating the heatmaps. If samples are more than 100, then sample labels are being omitted
-if (length(groups) < 100) {
+if (length(groups) <= 100) {
 	# Heatmap of top selected normalised genes
-	heatmaply(highly_variable, file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.html",sep =""),
-          limits = NULL, xlab = "Samples", ylab = "Genes", colors = brewer.pal(11,"Spectral"), scale = "row",
-	        main = paste("Top",nrow(highly_variable),"most variable genes across samples",sep=" "), key.title=NULL, 
-	        col_side_colors = data.frame(groups), hide_colorbar = FALSE, column_text_angle=60, fontsize_col = 9,
-	        fontsize_row = 8,  showticklabels=c(TRUE,TRUE))
-  heat_map <- heatmaply(highly_variable, #file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.png",sep =""),
-          limits = NULL, xlab = "Samples", ylab = "Genes", colors = brewer.pal(11,"Spectral"), scale = "row",
-          main = paste("Top",nrow(highly_variable),"most variable genes across samples",sep=" "), key.title=NULL, 
-          col_side_colors = data.frame(groups), hide_colorbar = FALSE, column_text_angle=60, fontsize_col = 9,
-          fontsize_row = 8,  showticklabels=c(TRUE,TRUE))
-  
-
-  } else {
-	# Heatmap of top selected genes, No sample-names 
-	heatmaply(highly_variable, # file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.html",sep =""),
-	          limits = NULL, xlab = "Samples", ylab = "Genes", colors = brewer.pal(11, "Spectral"), scale = "row",
-	          main = paste("Top",nrow(highly_variable),"most variable genes across samples",sep=" "), key.title=NULL, 
-	          col_side_colors = data.frame(groups), hide_colorbar = FALSE, showticklabels = c(FALSE, TRUE), fontsize_row = 8)
-  heat_map <- heatmaply(highly_variable, #file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.png",sep =""),
-            limits = NULL, xlab = "Samples", ylab = "Genes", colors = brewer.pal(11, "Spectral"), scale = "row",
-            main = paste("Top",nrow(highly_variable),"most variable genes across samples",sep=" "), key.title=NULL, 
-            col_side_colors = data.frame(groups), hide_colorbar = FALSE, showticklabels = c(FALSE, TRUE), fontsize_row = 8)
-  }
+	heatmaply(top_genes, file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.html",sep =""),
+          limits = NULL, colors = brewer.pal(11,"Spectral"), scale = "row", main = heatmap_title, 
+          key.title=NULL, col_side_colors = data.frame(groups), hide_colorbar = FALSE, 
+          column_text_angle=60, fontsize_col = 9, fontsize_row = 8,  showticklabels=c(TRUE,TRUE))
+  heat_map <- heatmaply(top_genes, limits = NULL, colors = brewer.pal(11,"Spectral"), scale = "row", 
+              main = heatmap_title, key.title=NULL, col_side_colors = data.frame(groups), 
+              hide_colorbar = FALSE, column_text_angle=60, fontsize_col = 9, fontsize_row = 8,  
+              showticklabels=c(TRUE,TRUE))
+} else {
+# Heatmap of top selected genes, No sample-names 
+heatmaply(top_genes, file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.html",sep =""),
+          limits = NULL, colors = brewer.pal(11, "Spectral"), scale = "row", main = heatmap_title, 
+          key.title=NULL, col_side_colors = data.frame(groups), hide_colorbar = FALSE, 
+          showticklabels = c(FALSE, TRUE), fontsize_row = 8)
+heat_map <- heatmaply(top_genes, limits = NULL, colors = brewer.pal(11, "Spectral"), scale = "row", 
+            main = heatmap_title, key.title=NULL, col_side_colors = data.frame(groups), 
+            hide_colorbar = FALSE, fontsize_row = 8, showticklabels = c(FALSE, TRUE))
+}
 
 plotly_IMAGE(heat_map, width = 1200, height = 800, format = "png", out_file = paste(outdir,"/",basename,"_heatmap_normalLog2CPM.png",sep =""))
 
